0ca65503c5a98c6dc9efab60079c50bb4ea98f2d
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-13 17:45:49 -0700

    step 1 completed

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
 
-const APP_NAME = "Help!";
+//metadata
+const APP_NAME = "Painote: Paint and Note Application";
 const app = document.querySelector<HTMLDivElement>("#app")!;
-
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+//page title
+const title = document.createElement("h1");
+title.innerHTML = "Painote: Paint and Note Application";
+app.append(title);
+
+//canvas
+const canvas = document.createElement("canvas") as HTMLCanvasElement;
+canvas.width = 256;
+canvas.height = 256
+app.append(canvas);
+const ctx = canvas.getContext("2d");
+if(ctx){
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 256, 256);
+}
+
+
