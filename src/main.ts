1b6409636ff044f79a3bb98057340ace11487053
ljcui1 <yangyangjiaocui@gmail.com>
2024-10-13 18:09:02 -0700

    step 2 completed

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,40 @@ canvas.width = 256;
 canvas.height = 256
 app.append(canvas);
 const ctx = canvas.getContext("2d");
-if(ctx){
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, 256, 256);
-}
 
+//drawing
+const cursor = {
+  active: false,
+  x: 0,
+  y: 0
+};
 
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if(cursor.active){
+    ctx?.beginPath();
+    ctx?.moveTo(cursor.x, cursor.y);
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+//buttons
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
